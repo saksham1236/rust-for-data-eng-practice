53d3c511201615ee1e388a11839ebcb625426565
Saksham Tandon <54137431+saksham1236@users.noreply.github.com>
2025-04-01 04:12:07 -0400

    Updated cli-fruit-salad/lib.rs

--- cli-fruit-salad/src/lib.rs
+++ cli-fruit-salad/src/lib.rs
@@ -2,19 +2,19 @@ use rand::{rng, seq::SliceRandom};
 
 pub fn create_fruit_salad(num_fruits: usize) -> Vec<String> {
     let mut fruits  = vec![
-        "Apple".to_string(),
-        "Banana".to_string(),
-        "Cherry".to_string(),
-        "Date".to_string(),
-        "Elderberry".to_string(),
-        "Fig".to_string(),
-        "Grape".to_string(),
-        "Honeydew".to_string(),
-        "Kiwi".to_string(),
+        String::from("Apple"),
+        String::from("Banana"),
+        String::from("Cherry"),
+        String::from("Date"),
+        String::from("Elderberry"),
+        String::from("Fig"),
+        String::from("Grape"),
+        String::from("Honeydew"),
+        String::from("Kiwi"),
     ];
     let mut rng = rng();
     fruits.shuffle(&mut rng);
 
-    fruits[..num_fruits].to_vec()   
+    fruits[..num_fruits].to_vec()
 }
 
