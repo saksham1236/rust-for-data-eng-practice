27810fa9365b808aa3fb97057f97d55e11fb2a0e
Saksham Tandon <54137431+saksham1236@users.noreply.github.com>
2025-04-06 15:54:13 -0400

    Adding finished graph example

--- graph-based-analysis/src/main.rs
+++ graph-based-analysis/src/main.rs
@@ -28,19 +28,20 @@ fn add_edge(graph: &mut UnGraph<&Fighter, f32>, nodes: &[NodeIndex], a: usize, b
 fn main() {
     let mut graph = UnGraph::new_undirected();
 
-    let fighters = {
+    let fighters = [
         Fighter::new("Dustin Poirier"),
         Fighter::new("Justin Gaethje"),
         Fighter::new("Conor McGregor"),
         Fighter::new("Khabib Nurmagomedov"),
         Fighter::new("Max Holloway"),
-    };
+    ];
 
-    let fighter_nodes = fighters
+    //Need to use Vec<NodeIndex> to store the nodes and fix iter() fixedSize implementation issue.
+    let fighter_nodes: Vec<NodeIndex> = fighters
         .iter()
         .map(|fighter| graph.add_node(fighter))
         .collect();
-
+    //Emulating Social Media or Network Analysius like a graph
     add_edge(&mut graph, &fighter_nodes, 0, 1);
     add_edge(&mut graph, &fighter_nodes, 1, 3);
     add_edge(&mut graph, &fighter_nodes, 2, 3);
@@ -50,7 +51,18 @@ fn main() {
     add_edge(&mut graph, &fighter_nodes, 3, 4);
 
     for (i, &node) in fighter_nodes.iter().enumerate() {
-        let name = &fighter[i].name;
-        let degree = graph.edges_directed(node, dir)
+        let name = &fighters[i].name;
+        let degree = graph.edges_directed(node, Direction::Outgoing).count() as f32;
+        let closeness = 1.0/degree;
+        println!("The closeness centrality of {} is {:.4}", name, closeness);
+
+        match name.as_str() {
+            "Dustin Poirier" => println!("{} is a great fighter with centrality {}", name, closeness),
+            "Justin Gaethje" => println!("{} is a great fighter with centrality {}", name, closeness),
+            "Conor McGregor" => println!("{} is a great fighter with centrality {}", name, closeness),
+            "Khabib Nurmagomedov" => println!("{} is a great fighter with centrality {}", name, closeness),
+            "Max Holloway" => println!("{} is a great fighter with centrality {}", name, closeness),
+            _ => println!("{} is not a fighter", name),
+        }
     }
 }
